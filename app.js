ea9d9940aeb50693c874078044d89cbe06fd3f78
lashon74 <lashonhud@gmail.com>
2020-09-21 16:39:01 -0400

    Update app.js

--- app.js
+++ app.js
@@ -66,9 +66,3 @@ app.post("/failure", function(req, res){
 app.listen(process.env.PORT || 3000, function(){
   console.log("We heard you on server 3000");
 });
-//
-// {"name":"Freddie'\''s Favorite Hats","contact":{"company":"Mailchimp","address1":"675 Ponce De Leon Ave NE","address2":"Suite 5000","city":"Atlanta","state":"GA","zip":"30308","country":"US","phone":""},"permission_reminder":"You'\''re receiving this email because you signed up for updates about Freddie'\''s newest hats.","campaign_defaults":{"from_name":"Freddie","from_email":"freddie@freddiehats.com","subject":"","language":"en"},"email_type_option":true}
-// API key
-// 0b411bb2c0a2b7c3200b21401ede35b4-us17
-// List id
-// 88b45e3f44
